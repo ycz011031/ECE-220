6f8a15d610a8ad59b48464b547fadbe7e8117b25
Yicheng Zhou <yz69@illinois.edu>
2022-06-27 17:40:48 -0500

    comment added mp2

--- mp02/prog2.asm
+++ mp02/prog2.asm
@@ -1,31 +1,31 @@
 ;netid : yz69
 ;
-;
+;the program under "Main" label is used for getting the input and decide whether to end the program for "=" condition
 .ORIG x3000
 MAIN
    GETC
    OUT
 ;check for "="
-   LD R1,ASCIIEQUA
-   JSR NEGATIVE
-   ADD R2,R1,R0
-   BRZ EQUALDET   
-   BRnzp EVALUATE
+   LD R1,ASCIIEQUA    ;load in the ascii value for "="
+   JSR NEGATIVE       
+   ADD R2,R1,R0       ;adding the inputed value and the inverted ascii for "ii"
+   BRZ EQUALDET       ;if the previous step returned zero, meaning a "=" is entered
+   BRnzp EVALUATE     ;if a "=" is not entered jump to evaluate
 EQUALDET
-   LD R4,STACK_TOP 
-   LD R1,STACK_START
-   JSR NEGATIVE
-   ADD R4,R1,R4
-   ADD R4,R4,#1 
+   LD R4,STACK_TOP    ;load in the current stack top
+   LD R1,STACK_START  ;load in the stack start
+   JSR NEGATIVE       
+   ADD R4,R1,R4       ;adding the stack top and inverted stack top
+   ADD R4,R4,#1       ;i don't know why, but this step is required for the program to function
    BRz SKIP7
-   JSR INVALID
+   JSR INVALID        ;reaching this step means the stack more or less than 1 value
 SKIP7
-   JSR POP
-   ADD R3,R0,#0
+   JSR POP            
+   ADD R3,R0,#0       ;loading the numerical value into r3 for print_hex function
    JSR PRINT_HEX
 
 
-
+;this is a subroutine for printing "invalid expression" and halting the program
 INVALID
    LEA R0, PRITINVAL
    PUTS
@@ -35,6 +35,8 @@ INVALID
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;R3- value to print in hexadecimal
+;this part is directly coppied from mp1, some of the registers has been switched around to accomamdate the changes.
+;in the line by line comment for the code, r3 is changed to r1, r4 is changed to r3, temp register is also changed
 PRINT_HEX
    LD R4,COUNTDOWN     ; load in counter 4, for 16bit bi- 4bit hex conversion
 BTHLP     
@@ -71,56 +73,55 @@ SKIP15
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;R0 - character input from keyboard
 ;R6 - current numerical output
-;
-;
+;the evaluate program has two main part
+;first of which is to check whether the input is a number under check num to translating num char from ascii to decimal
+;second is the check if the input is one of "+" , "-" ,"/","*","^" using the same method for the "=" check
 EVALUATE
-    ST R7,SAVEEVR7 ;
-    ADD R0,R0,#0   ;
-
-;your code goes here
+    ST R7,SAVEEVR7 ; due to multiple subroutines is called during the excution of this one, a callee save of R7 is done here
+    ADD R0,R0,#0   ; setting cc
 ; check space
-    LD R1,ASCIISPAC ;
+    LD R1,ASCIISPAC ; 
 	JSR NEGATIVE    ;
 	ADD R2,R1,R0    ;
 	BRz INPUT       ;
 
 ; check num 
-    LD R1,ASCIIZERO ;
+    LD R1,ASCIIZERO ; checking if the ascii for input is lower than "0"
 	JSR NEGATIVE    ;
 	ADD R2,R0,R1    ;
-	BRn SKIP1       ;
-	LD R1,ASCIININE ;
+	BRn SKIP1       ; if yes than skip to check operants
+	LD R1,ASCIININE ; checking if the ascii for input is higher than "9"
 	JSR NEGATIVE    ;
 	ADD R2,R0,R1    ;
-	BRp SKIP1       ;
+	BRp SKIP1       ; if yes than skip to check operants
 ; translating num char from ascii to decimal	
-	LD R1,ASCIIZERO ;
+	LD R1,ASCIIZERO ; subtracting the ascii offset from the input
 	JSR NEGATIVE    ;
 	ADD R0,R1,R0    ;
 	JSR PUSH        ;
-	BRnzp INPUT     ;
+	BRnzp INPUT     ; when push is done return to main
 SKIP1
-    ST R0,R0SAVE1
-	JSR POP         ;
-	ADD R4,R0,#0    ;
+    ST R0,R0SAVE1   ; reaching this part means that the input is either an operant or invalid
+	JSR POP         ; thus we pop twice and load each pop into r3 and r4
+	ADD R4,R0,#0    ; beofre the operation begain we first save R0 as input ascii for futrue use
 	JSR POP         ;
 	ADD R3,R0,#0    ;
 	LD R0,R0SAVE1   ;
 	ADD R5,R5,#0    ;
-	BRnz SKIP0      ;
-	JSR INVALID     ;
+	BRnz SKIP0      ; checking if R5 is 1
+	JSR INVALID     ; if r5 is 1 then input is invalid
 SKIP0
 ; check "+"
-    LD R1,ASCIIPLUS ;
-	JSR NEGATIVE    ;
-	ADD R2,R1,R0    ;
-	BRnp SKIP2      ;
+    LD R1,ASCIIPLUS ; the check oeprant funciton is same as check "="
+	JSR NEGATIVE    ; when an operant is caught, we call for the respective calc function
+	ADD R2,R1,R0    ; and when the subroutine is completed we call for push
+	BRnp SKIP2      ; and return to main
 	JSR PLUS        ;
 	JSR PUSH        ;
-	BRnzp INPUT     ;
+	BRnzp INPUT     ; this is input instead of main due to changed structure for the program
 ; check "-"
 SKIP2     
-    LD R1,ASCIIMINU ;
+    LD R1,ASCIIMINU ; same as check "+"
 	JSR NEGATIVE    ;
 	ADD R2,R1,R0    ;
 	BRnp SKIP3      ;
@@ -129,7 +130,7 @@ SKIP2
 	BRnzp INPUT     ;
 SKIP3
 ; check "*"
-    LD R1,ASCIIMULT ;
+    LD R1,ASCIIMULT ; same as check "+"
 	JSR NEGATIVE    ;
 	ADD R2,R1,R0    ;
 	BRnp SKIP4      ;
@@ -138,7 +139,7 @@ SKIP3
 	BRnzp INPUT     ;
 SKIP4
 ;check "/"
-    LD R1,ASCIIDIVI ;
+    LD R1,ASCIIDIVI ; same as check "+"
 	JSR NEGATIVE    ;
 	ADD R2,R1,R0    ;
 	BRnp SKIP5      ;
@@ -147,15 +148,15 @@ SKIP4
 	BRnzp INPUT     ;
 SKIP5
 ; check "^"
-    LD R1,ASCIIEXPO ;
+    LD R1,ASCIIEXPO ; same as check "+"
 	JSR NEGATIVE    ;
 	ADD R2,R1,R0    ;
-	BRnp SKIP6
+	BRnp SKIP6      ; reaching skip6 means that the input meaning that the input is either a num or operant
 	JSR EXP         ;
 	JSR PUSH        ;
 	BRnzp INPUT     ;
 SKIP6
-    JSR INVALID
+    JSR INVALID     ;thus the invalid subroutin is called
 
 INPUT
      BRnzp MAIN  
@@ -163,7 +164,7 @@ INPUT
 SAVEEVR7 .BLKW #1    ;
 R0SAVE1  .BLKW #1    ;
 	
-	
+; this is a subroutine for inverting a value, mainly used in evaluate
 NEGATIVE	
 	NOT R1,R1       ;
 	ADD R1,R1,#1    ;
@@ -203,9 +204,9 @@ MULDONE
 ;input R3, R4
 ;out R0
 DIV	
-   AND R0,R0,#0   ;
-   NOT R4,R4      ;
-   ADD R4,R4,#1   ;
+   AND R0,R0,#0   ; this function is realized by subtracting R3 by R4
+   NOT R4,R4      ; and r0 plus each time the subtraction happened
+   ADD R4,R4,#1   ; until r3 reaches zero or negative
 DIVLP
    ADD R3,R3,R4   ;
    BRn DIVDONE    ;
@@ -217,11 +218,11 @@ DIVDONE
 ;input R3, R4
 ;out R0
 EXP
-   ADD R0,R3,#0
+   ADD R0,R3,#0   ;this function is realized by creating an outer and inner loop
 EXLP   
-   ADD R2,R4,#-1
-   BRnz EXD
-   ADD R5,R3,#0
+   ADD R2,R4,#-1  ;the inner loop is similer to the mult function
+   BRnz EXD       ;each time the inner loop is reached, we set one of the operater as the result for previous looping
+   ADD R5,R3,#0   ;the outer loop would stop onece the R4 is decrementted to 0
    ADD R2,R0,#0
 MULP   
    ADD R6,R5,#-1
