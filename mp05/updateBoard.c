5cd6248a9784e296fe304f7f725c83ce5ceccc3d
Yicheng Zhou <yz69@illinois.edu>
2022-07-18 18:48:17 -0500

    mp05 coding done

--- mp05/updateBoard.c
+++ mp05/updateBoard.c
@@ -13,7 +13,24 @@
  */
 
 int countLiveNeighbor(int* board, int boardRowSize, int boardColSize, int row, int col){
+    int i,j;
+    int cnt=0;
+    for (i=row-1;i<=row+1;i++){
+        if(i>=0 && i<boardRowSize){
+            for (j=col-1;j<=col+1;j++){
+                if(j>=0 && j<boardColSize){
+                    if(board[i*boardColSize+j]==1 && (i*boardColSize+j != row*boardColSize+col)){
+                        cnt = cnt +1 ;
+                    }
+                }
+            }
+        }
+
+    }
+    return cnt;
 }
+
+
 /*
  * Update the game board to the next step.
  * Input: 
@@ -24,6 +41,30 @@ int countLiveNeighbor(int* board, int boardRowSize, int boardColSize, int row, i
  * Output: board is updated with new values for next step.
  */
 void updateBoard(int* board, int boardRowSize, int boardColSize) {
+    int a;
+    int b;
+    int index;
+    int updatedBoard[boardColSize*boardRowSize];
+    for (a=0;a<boardRowSize;a++){
+        for (b=0;b<boardColSize;b++){
+            index= a*boardColSize+b;
+            if (countLiveNeighbor(board,boardRowSize,boardColSize,a,b)==3){
+                updatedBoard[index] = 1;
+            }
+            else if (countLiveNeighbor(board,boardRowSize,boardColSize,a,b)==2 && board[index]==1 ){
+                updatedBoard[index] = 1;
+            }
+            else{
+                updatedBoard[index] = 0;
+            }
+        }
+
+    }
+
+    int l;
+    for (l=0;l < boardColSize*boardRowSize; l++){
+        board[l]=updatedBoard[l];
+    }
 }
 
 /*
@@ -38,6 +79,20 @@ void updateBoard(int* board, int boardRowSize, int boardColSize) {
  * return 0 if the alive cells change for the next step.
  */ 
 int aliveStable(int* board, int boardRowSize, int boardColSize){
+    int x ;
+    int y ;
+    int stcheck;
+    for (x=0;x<boardRowSize;x++){
+        for (y=0;y<boardColSize;y++){
+            stcheck = countLiveNeighbor(board,boardRowSize,boardColSize,x,y);
+            if ((stcheck < 2 || stcheck >3) && board[x*boardColSize+y]){
+                return 0;
+            }
+
+        }
+    }
+    return 1;
+
 }
 
 				
