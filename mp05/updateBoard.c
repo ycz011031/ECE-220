054ea2b89017909466871e8d8ed97f4cd30767ae
Yicheng Zhou <yz69@illinois.edu>
2022-07-19 15:44:07 -0500

    comment added mp05

--- mp05/updateBoard.c
+++ mp05/updateBoard.c
@@ -1,3 +1,10 @@
+// This program is used to realize the game of life
+//this particular file houses the three main function of the program
+// int countliveneighbor counts the number of live cell serrounding an input
+// void updateboard updates the board according the rule
+// the alivestable checks if a specific layout is stable
+
+
 /*
  * countLiveNeighbor
  * Inputs:
@@ -12,9 +19,14 @@
  * Pay attention for the edge and corner cells, they have less neighbors.
  */
 
+
+// this function uses an outer an inner loop to loop through row and col
+// it will discount the selected cell
 int countLiveNeighbor(int* board, int boardRowSize, int boardColSize, int row, int col){
-    int i,j;
+    int i,j; 
+    // loop counters
     int cnt=0;
+    // counts of alive cell
     for (i=row-1;i<=row+1;i++){
         if(i>=0 && i<boardRowSize){
             for (j=col-1;j<=col+1;j++){
@@ -40,10 +52,18 @@ int countLiveNeighbor(int* board, int boardRowSize, int boardColSize, int row, i
  * boardColSize: the number of cols on the game board.
  * Output: board is updated with new values for next step.
  */
+
+
+// this function checks if the alive neignbor is 3, if so than cell will be alive
+// then it chekcs if the alive neighbor is 2 while if the cell is alive.
+// else the cell will be dead
 void updateBoard(int* board, int boardRowSize, int boardColSize) {
     int a;
+    //row counter
     int b;
+    //col counter
     int index;
+    // a computed value for array index
     int updatedBoard[boardColSize*boardRowSize];
     for (a=0;a<boardRowSize;a++){
         for (b=0;b<boardColSize;b++){
@@ -80,13 +100,20 @@ void updateBoard(int* board, int boardRowSize, int boardColSize) {
  */ 
 int aliveStable(int* board, int boardRowSize, int boardColSize){
     int x ;
+    // row counter
     int y ;
+    // col counter
     int stcheck;
+    // stcheck is used to store value of aliveneighbor call
     for (x=0;x<boardRowSize;x++){
         for (y=0;y<boardColSize;y++){
             stcheck = countLiveNeighbor(board,boardRowSize,boardColSize,x,y);
             if ((stcheck < 2 || stcheck >3) && board[x*boardColSize+y]){
                 return 0;
+                // if each live cell is surrounded by 2 or 3 live cell it stays alive
+                // thus if there is one live cell that doesn't satisfy above mentioned
+                // this board is not stable
+                // otherwise the board is stabel
             }
 
         }
