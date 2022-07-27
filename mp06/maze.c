1bd65311124cba0a1b922db23992c94ec2c7c6ca
Yicheng Zhou <yz69@illinois.edu>
2022-07-26 19:18:48 -0500

    comment added mp06

--- mp06/maze.c
+++ mp06/maze.c
@@ -10,6 +10,13 @@
  * RETURN:       A filled maze structure that represents the contents of the input file
  * SIDE EFFECTS: None
  */
+
+ // in this program, four function are realized
+ // first of which would read an input as txt file and extract the maze in it
+ // second is given the pointer destroy every thing in the maze sturcutre
+ // third is given a maze structure, print out the maze
+ // is using recursive function find the solution for said maze
+
 maze_t * createMaze(char * fileName)
 {
 
@@ -18,6 +25,7 @@ maze_t * createMaze(char * fileName)
     FILE*file = fopen(fileName,"r");//opens the text file in reading mode
     fscanf (file,"%d",&cols);//scan the width of the maze(num of cols)
     fscanf (file,"%d\n",&rows);//scan the height of the maze(num of rows)
+    //the line change charc caused me so much problem at the begining
     //allocating memory for cells
     maze_t * maze = (maze_t*)malloc(sizeof(maze_t));
     maze->cells = calloc(rows,sizeof(char*));//allocates space for pointer of each column
@@ -77,7 +85,7 @@ void destroyMaze(maze_t * maze)
         free(maze->cells[i]);
     }
     free(maze->cells);
-    free(maze); 
+    free(maze); //as you can see, i used the free function in reverse order of malloc funciton
     // Your code here.
 }
 
@@ -90,6 +98,9 @@ void destroyMaze(maze_t * maze)
  * RETURN:       None
  * SIDE EFFECTS: Prints the maze to the console
  */
+
+// this is quite a simple function, 2 loops through the two indexes of a 2d array
+// x,y are temp variable to hold rows and cols from previous funciton
 void printMaze(maze_t * maze)
 {
     int x,y,i,j;
@@ -114,25 +125,39 @@ void printMaze(maze_t * maze)
  * RETURNS:              0 if the maze is unsolvable, 1 if it is solved
  * SIDE EFFECTS:         Marks maze cells as visited or part of the solution path
  */ 
+
+// in this function i roughly followed the algraithem given by the mp insturction page
+// slight modification was made in effert to debug
+// although i can see some code can be more optimized, i don't want to change anything to avoid bugs
+
+
 int solveMazeDFS(maze_t * maze, int col, int row)
 {
     if(col <0 || row <0 || col >= maze->width || row >= maze->height){
         return 0;
     }
+    //checking if input is out of bound
     if(maze->cells[row][col] != ' ' && maze->cells[row][col] != 'S' && maze->cells[row][col] != 'E'){
         return 0;
     }
+    //checkigng if input is a wall
     if(maze->cells[row][col] == 'E'){
         return 1;
     }
+    //checking if input is the end
+    //i understand  that by placing this in front ' ' check would save a condition for said funciton
+
  	
     if(maze->cells[row][col] != 'S'){
         maze->cells[row][col] = '*';
     }  
+    // to avoid 'S' to be overwritten
 
 
      
-        
+    // i am not 100% sure why this is the case
+    //by swtiching around the checking order, i fix some bugs where a singel '*' would "leak" from S
+    // but this code works so.....   
 	
     if(solveMazeDFS(maze,col+1,row)){
         return 1;
@@ -157,6 +182,7 @@ int solveMazeDFS(maze_t * maze, int col, int row)
     if(maze->cells[row][col] != 'S'){
         maze->cells[row][col] = '~';
     }
+    // same as before, preventing S to be overwritten.
 
     // Your code here. Make sure to replace following line with your own code.
     return 0;
